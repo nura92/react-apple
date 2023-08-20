b344660f7d2f0c22954995dfd4a0557437a66d01
nura92 <131611344+nura92@users.noreply.github.com>
2023-08-20 05:33:29 -0700

    this is react apple website project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Headers from "./components/headers";
+import Footer from "./components/footer";
+import AlertSection from "./components/alertSection";
+import FirstSection from "./components/firstSection";
+import SecondSection from "./components/secondSection";
+import ThirdSection from "./components/thirdSection";
+import FourthSection from "./components/fourthSection";
+import FivthSection from "./components/fivthSection";
+import SixSection from "./components/sixSection";
+import "./css/bootstrap.css";
+import "./css/styles.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Headers />
+			<AlertSection />
+			<FirstSection />
+			<SecondSection />
+			<ThirdSection />
+			<FourthSection />
+			<FivthSection />
+			<SixSection />
+			<Footer />
+		</div>
+	);
 }
-
 export default App;
